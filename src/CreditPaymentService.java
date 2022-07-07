cf3718d000f4c4c75d1ee7b6fe694898023761ea
Levon Poghosyan <levon9393@bk.ru>
2022-07-07 13:23:49 +0300

    First commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,11 @@
+public class CreditPaymentService {
+    public int calculate(int x, int y, double z) {
+
+        double bet = z / 100 / 12;
+        y = y * 12;
+        double result = x * (bet + bet/(Math.pow((1+bet),y) - 1));
+
+        return (int) result;
+    }
+
+}
